f55192896911c24d6ab8d8597068482f5ac06c09
Miriam-Frieder <m0556777068@gmail.com>
2025-04-22 22:18:15 +0300

    finish login with admin check

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from './user.service';
-import { map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, tap, throwError } from 'rxjs';
 
 
 @Injectable({
@@ -10,18 +10,25 @@ import { map, Observable, tap } from 'rxjs';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient,private userService: UserService) {}
-    login(username: string, password: string) :Observable<boolean> {
-     return this.http.post(`${environment.apiBaseUrl}/auth/login`, { userNameOrEmail: username, password })
-        .pipe(
-          tap((response: any) => {
+  constructor(private http: HttpClient, private userService: UserService) { }
+  login(username: string, password: string): Observable<boolean> {
+    return this.http.post(`${environment.apiBaseUrl}/auth/login`, { userNameOrEmail: username, password })
+      .pipe(
+        tap((response: any) => {
+          if (response && response.user.roles.includes('Admin')) {
             console.log('Login response:', response);
-            if (response && response.token) {
-              sessionStorage.setItem('token', response.token); 
-              this.userService.setCurrentUser(response.user);
-            }
-          }),
-          map((response: any) => !!response?.token)
-        );
-}
+            sessionStorage.setItem('token', response.token);
+            this.userService.setCurrentUser(response.user);
+          } else {
+            console.error('Login failed: User is not an admin');
+            throw new Error('User is not an admin');
+          }
+        }),
+        map((response: any) => !!response?.token), // Map response to a boolean
+        catchError((err) => {
+          console.error('Login failed:', err.message || err);
+          return throwError(() => err); // Re-throw the error for further handling
+        })
+      );
+  }
 }
